dd1af52236323adc28ff6b9af039cd1e702a9e91
4Furki4 <muhammedcengiz1@gmail.com>
2022-06-05 17:58:58 +0300

    GenreOperations's been dealt with. Comments in MappingProfiles updated.

--- Common/MappingProfile.cs
+++ Common/MappingProfile.cs
@@ -1,9 +1,11 @@
 using AutoMapper;
-using GameStore.Application.GameOperations.Command.Create;
 using GameStore.Application.GameOperations.Command.Update;
 using GameStore.Application.GameOperations.Query.GetGameDetail;
 using GameStore.Application.GameOperations.Query.GetGames;
+using GameStore.Application.GenreOperations.Query.GetDetail;
 using GameStore.Entities;
+using GameStore.Application.GenreOperations.Query.Get;
+using GameStore.Application.GenreOperations.Command.Create;
 
 namespace GameStore.Common
 {
@@ -22,15 +24,13 @@ namespace GameStore.Common
             .ForMember(dest => dest.Genres,opt => opt.MapFrom(src => src.GameGenres.Select(n=>n.Genre.Name)))
             .ForMember(dest=>dest.Writers,opt=>opt.MapFrom(src=>src.GameWriters.Select(s=>s.Writer.Name).ToList()));
 
-            // CreateMap<CreateGameModel,Game>()
-            // .ForMember(dest=>dest.Name,opt=>opt.MapFrom(src=>src.Name))
-            // .ForMember(dest=>dest.Price,opt=>opt.MapFrom(src=>src.Price))
-            // .ForMember(dest=>dest.PublishDate,opt=>opt.MapFrom(src=>src.PublishDate));
-            // .ForPath(dest=>dest.GameGenres.Select(g=>g.GenreID),opt=>opt.MapFrom(src=>src.GameGenres))
-            // .ForPath(dest=>dest.GameDevelopers.Select(g=>g.DeveloperID),opt=>opt.MapFrom(src=>src.GameDevelopers))
-            // .ForPath(dest=>dest.GameWriters.Select(g=>g.WriterID),opt=>opt.MapFrom(src=>src.GameWriters))
-
             CreateMap<UpdateGameModel,Game>();
+
+            CreateMap<Genre,GenreViewModel>();
+
+            CreateMap<Genre,GenreDetailViewModel>();
+
+            CreateMap<CreateGenreModel, Genre>();
         }
     }
 }
